3ab763c9df8664d07200b409480786a5967fa65b
Vigor Akbar <vigorakbar@gmail.com>
2019-09-19 01:03:34 +0700

    restructure app

---
+++ src/containers/app/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from 'containers/header/Header';
+import Main from 'containers/main/Main';
+import Footer from 'containers/footer/Footer';
+
+const App = () => (
+  <React.Fragment>
+    <Header />
+    <Main />
+    <Footer />
+  </React.Fragment>
+);
+
+export default App
