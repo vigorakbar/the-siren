42ba457784ce0a57e48428ceddfaa8430854dccb
Vigor Akbar <vigorakbar@gmail.com>
2020-12-31 16:26:17 +0700

    Updates

---
+++ precache-manifest.79edda746668d79b75764b1b76e12bc6.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63d2f57bdd230e20b5da312119c4fc48",
+    "url": "/the-siren/index.html"
+  },
+  {
+    "revision": "8de145d97b1119d2a46f",
+    "url": "/the-siren/static/css/main.d51cb0c2.chunk.css"
+  },
+  {
+    "revision": "83f1285641b11a7b3d3a",
+    "url": "/the-siren/static/js/2.2145f0c8.chunk.js"
+  },
+  {
+    "revision": "8de145d97b1119d2a46f",
+    "url": "/the-siren/static/js/main.e825345a.chunk.js"
+  },
+  {
+    "revision": "0d7eab9b829898693b7a",
+    "url": "/the-siren/static/js/runtime~main.581756f8.js"
+  },
+  {
+    "revision": "7d8d881d61d1478ceb5ea35068f31956",
+    "url": "/the-siren/static/media/asideCarousel1.7d8d881d.png"
+  },
+  {
+    "revision": "0039c90bfa0a437f4ab9d810d70e1658",
+    "url": "/the-siren/static/media/asideCarousel2.0039c90b.png"
+  },
+  {
+    "revision": "a75346a6e63c906becf465af1ba96b03",
+    "url": "/the-siren/static/media/carousel1.a75346a6.png"
+  },
+  {
+    "revision": "4a76763cb941b94c5f5b4e052a0f99ec",
+    "url": "/the-siren/static/media/carousel2.4a76763c.png"
+  },
+  {
+    "revision": "91d416f7999da01cf7148d6de42a5132",
+    "url": "/the-siren/static/media/carousel3.91d416f7.png"
+  },
+  {
+    "revision": "f56d3dc2cb430b2e3acfc2c5a09a1391",
+    "url": "/the-siren/static/media/carousel4.f56d3dc2.png"
+  },
+  {
+    "revision": "30e4c7ce04f1301d2b1458ca46a825a1",
+    "url": "/the-siren/static/media/facebook.30e4c7ce.svg"
+  },
+  {
+    "revision": "63792d511fae0e3272e3b51e8d7dfb24",
+    "url": "/the-siren/static/media/instagram.63792d51.svg"
+  },
+  {
+    "revision": "6e7f36b93ea73e750c717e7d288ee645",
+    "url": "/the-siren/static/media/latest1.6e7f36b9.png"
+  },
+  {
+    "revision": "5d0be437b86c99539944f4e866fd01fd",
+    "url": "/the-siren/static/media/latest2.5d0be437.png"
+  },
+  {
+    "revision": "feff05b3973cb70d4f86ab25e9d0f27e",
+    "url": "/the-siren/static/media/latest3.feff05b3.png"
+  },
+  {
+    "revision": "594a13201ffdced44f6e772578a80c7d",
+    "url": "/the-siren/static/media/more.594a1320.svg"
+  },
+  {
+    "revision": "0caaa4886aa483bbf07edf76c162fd1e",
+    "url": "/the-siren/static/media/oval.0caaa488.svg"
+  },
+  {
+    "revision": "95ddc1005792d46198ba399f8d4fd2de",
+    "url": "/the-siren/static/media/twitter.95ddc100.svg"
+  }
+]);
